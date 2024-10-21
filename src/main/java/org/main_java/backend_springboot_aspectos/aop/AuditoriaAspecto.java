cb44d8854ecc9a6f0e4122b637c3d2fecc6a3b0d
Jaime Lopez Diaz <jlopedia@myuax.com>
2024-10-21 23:13:19 +0200

    -

--- src/main/java/org/main_java/backend_springboot_aspectos/aop/AuditoriaAspecto.java
+++ src/main/java/org/main_java/backend_springboot_aspectos/aop/AuditoriaAspecto.java
@@ -1,7 +1,10 @@
 package org.main_java.backend_springboot_aspectos.aop;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.main_java.backend_springboot_aspectos.domain.EventoMagico;
 import org.main_java.backend_springboot_aspectos.service.AuditoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,12 +14,18 @@ import org.springframework.stereotype.Component;
 @Component
 public class AuditoriaAspecto {
 
-    @Autowired
-    private AuditoriaService auditoriaService;
 
-    @AfterReturning(pointcut = "execution(* org.main_java.backend_springboot_aspectos.service.HechizoService.lanzarHechizo(..))", returning = "eventoMagico")
-    public void registrarAuditoria(EventoMagico eventoMagico) {
-        auditoriaService.auditarEvento(eventoMagico);
+    @Before("execution(* org.main_java.backend_springboot_aspectos.service.AuditoriaService.auditarEvento(..)) && args(eventoMagico)")
+    public void antesDeAuditarEvento(JoinPoint joinPoint, EventoMagico eventoMagico) {
+        // Realizamos una acción antes de auditar el evento
+        System.out.println("Preparando para auditar el evento: " + eventoMagico.getDescripcion());
+
+        // Podrías acceder a más detalles del evento
+        System.out.println("Evento mágico lanzado por: " + eventoMagico.getLanzador().getNombre());
+
+        // Acceder al nombre del metodo que será interceptado
+        String methodName = joinPoint.getSignature().getName();
+        System.out.println("Interceptando el método: " + methodName);
     }
 }
 
--- src/main/java/org/main_java/backend_springboot_aspectos/aop/RegistroAspecto.java
+++ src/main/java/org/main_java/backend_springboot_aspectos/aop/RegistroAspecto.java
@@ -3,6 +3,7 @@ package org.main_java.backend_springboot_aspectos.aop;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.main_java.backend_springboot_aspectos.domain.Usuario;
+import org.main_java.backend_springboot_aspectos.domain.hechizos.Hechizo;
 import org.main_java.backend_springboot_aspectos.service.RegistroService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -14,9 +15,10 @@ public class RegistroAspecto {
     @Autowired
     private RegistroService registroService;
 
-    @AfterReturning("execution(* org.main_java.backend_springboot_aspectos.service.HechizoService.lanzarHechizo(..)) && args(usuario)")
-    public void registrarAccion(Usuario usuario) {
+    @AfterReturning("execution(* org.main_java.backend_springboot_aspectos.service.HechizoService.lanzarHechizo(..)) && args(usuario, hechizo)")
+    public void registrarAccion(Usuario usuario, Hechizo hechizo) {
         registroService.registrarAccion(usuario, "Lanzar Hechizo", "Éxito");
+        System.out.println("Hechizo lanzado: " + hechizo.getNombre());
     }
 }
 
